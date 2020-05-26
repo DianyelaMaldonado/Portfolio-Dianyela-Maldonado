41290bdd9312f660360faf94fa98db0342efcbbb
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-05-25 20:41:07 -0500

    I added changes in app.js scroll functionality in arrow of the first section

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 let arrow = document.getElementById('arrow');
 arrow.addEventListener('click', function(e) {
     e.preventDefault();
-    scroll(0, 860);
+    $('html, body').animate({
+        scrollTop: $(".tech-skills").offset().top
+    }, 1500);
 });
-
-// $("nav").click(function() {
-//     $("#navAnchor").animate({
-//         top: 200;
-//     });
-// };
